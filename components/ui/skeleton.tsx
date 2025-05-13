8aa8a3c5138818a8f824be8f60864772f9b3cb1a
DjamalDixneuf <clydedjamaldine@gmail.com>
2025-05-13 16:07:46 +0300

    Update skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,8 +1,7 @@
 import type React from "react"
-import { cn } from "@/lib/utils"
 
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("animate-pulse rounded-md bg-gray-800/50", className)} {...props} />
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Skeleton({ className = "", ...props }: SkeletonProps) {
+  return <div className={`animate-pulse rounded-md bg-gray-800/50 ${className}`} {...props} />
 }
-
-export { Skeleton }
