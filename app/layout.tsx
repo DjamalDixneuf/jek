a3776a1156beaa6e7e128a2594ed21c087c5a9ea
DjamalDixneuf <clydedjamaldine@gmail.com>
2025-05-14 12:36:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,26 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "../components/theme-provider"
+import "./styles/index.css"
+import "./styles/stylesA.css"
+import "./styles/admin-dashboard.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Jekle - Your Favorite Streaming Platform",
-  description: "Discover, watch, and request movies and series you love, all in one click, wherever you are!",
-  keywords: "streaming, films, séries, plateforme, demande de films, Jekle divertissement, Madagascar streaming",
+  title: "Jekle - Plateforme de Streaming",
+  description:
+    "Découvrez, regardez et demandez les films et séries que vous aimez, le tout en un clic, où que vous soyez !",
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider defaultTheme="dark">{children}</ThemeProvider>
-      </body>
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
